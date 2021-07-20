cbe7738c1c2f6424ce64053c726b6d6b46b04994
김혜지 <gpwl981207@naver.com>
2021-07-20 21:28:32 +0900

    0722-3 done

--- 0722-3/0722-3-Khj.playground/Contents.swift
+++ 0722-3/0722-3-Khj.playground/Contents.swift
@@ -1,28 +1,14 @@
 import Foundation
 
-func decimalToBinary(num: Int, count: Int) -> [Int] {
-    var decimal = num
-    var binary = Array(repeating: 0, count: count)
-    var n = 0
-    while decimal > 1 {
-        
-    }
-    
-    if decimal == 1 {
-        binary[count-1] = 1
-    }
-    return binary
-}
-
+/**
+ - zip : 두 요소를  나란히 튜플로 생성 -> 두 배열에서 같은 인덱스의 요소를 같이 사용하기 위해.
+ - String(_ : , radix: Int) : 진수 변환법 -> 진수 : 몇 수를 증가시켜 자릿수를 올릴건지에 대한 "단위"
+ - String(repeating: , count: ) : count만큼 반복해서 String으로 반환 -> OR 연산 결과의 비트 수가 n보다 적을 수 있으므로 확인.
+ */
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-    var answer: [String] = []
-    for i in 0...n {
-        var element1 = arr1[i]
-        var element2 = arr2[1]
-        
-        for i in 0...n {
-            
-        }
+    return zip(arr1, arr2).map{
+        var binary = String($0.0 | $0.1, radix: 2)
+        binary = binary.count < n ? String(repeating: "0", count: n - binary.count) + binary : binary
+        return binary.replacingOccurrences(of: "0", with: " ").replacingOccurrences(of: "1", with: "#")
     }
-    return answer
 }
