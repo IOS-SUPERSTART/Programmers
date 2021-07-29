045cb720499bc6f82b06bfa157ac9689ca5274d3
jihye <zehye.01@gmail.com>
2021-07-29 20:57:34 +0900

    0729-pjh

--- 0729/0729-2/0729-Pjh.playground/Contents.swift
+++ 0729/0729-2/0729-Pjh.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    var losted = lost.filter{!reserve.contains($0)}
+    var borrowes = reserve.filter{!lost.contains($0)}
+
+    var ans = n - losted.count
+
+    for i in 0..<losted.count {
+        var x: Int?
+
+        for i in 0..<borrowes.count {
+            if losted[i] == borrowes[i] - 1 || losted[i] == borrowes[i] + 1 {
+                x = i
+                break
+            }
+        }
+
+        if let index = x {
+            borrowes.remove(at: index)
+            ans += 1
+        }
+    }
+
+    return ans
+}
