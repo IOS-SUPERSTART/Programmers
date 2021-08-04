0b4a7a7bd3f30c14b26fdaac8e19e59ae2efad37
zehye <zehye.01@gmail.com>
2021-08-04 19:04:38 +0900

    문제 어려워어어어어

--- 0805/0805-3/0805-Pjh.playground/Contents.swift
+++ 0805/0805-3/0805-Pjh.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ nums: [Int]) -> Int {
+    
+    // 소수인지 확인하는 함수
+    func prime(_ n: Int) -> Bool {
+        for i in 2..<n {
+            if n%i == 0 {  // 나뉘면 소수 아니쥐
+                return false
+            }
+        }
+        return true
+    }
+    
+    var sum = [Int]()
+    
+    // 삼중 포문 말고 좋은 방법 있나....
+    for i in 0..<nums.count - 2 {
+        for j in i+1..<nums.count {
+            for k in j+1..<nums.count {
+                sum.append(nums[i] + nums[j] + nums[k])
+            }
+        }
+    }
+    
+    return sum.filter{prime($0)}.count
+}
