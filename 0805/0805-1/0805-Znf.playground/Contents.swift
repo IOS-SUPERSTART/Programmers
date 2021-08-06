8649ac5710081ae02e08cac155326a8417ab2446
znfod-dev <znfod.dev@gmail.com>
2021-08-06 10:45:28 +0900

    종현

--- 0805/0805-1/0805-Znf.playground/Contents.swift
+++ 0805/0805-1/0805-Znf.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+     
+    var arr = Array<(stage:Int, goal:Double, pass:Double)>()
+    for i in 0..<N {
+        arr.append((stage: i+1, goal: 0, pass: 0))
+    }
+    stages.forEach { stage in
+        for i in 0..<arr.count {
+            if arr[i].stage < stage {
+                arr[i].goal += 1
+                arr[i].pass += 1
+            }else if arr[i].stage == stage {
+                arr[i].goal += 1
+            }
+        }
+    }
+    arr.sort(by: { (($0.goal - $0.pass) / $0.goal) > (($1.goal - $1.pass) / $1.goal) } )
+    return arr.compactMap { $0.stage }
+}
--- 0805/0805-2/0805-Znf.playground/Contents.swift
+++ 0805/0805-2/0805-Znf.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func solution(_ dartResult:String) -> Int {
+    
+    let pattern1 = "10|[0-9]"
+    let pattern2 = "[SDT][*#]?"
+    let arr1 = dartResult.match(pattern1)
+    let arr2 = dartResult.match(pattern2)
+    var arr3 = Array<Int>(repeating: 0, count: 3)
+    for i in 0..<3 {
+        var result = 0
+        let val = arr2[i][0]
+        if val.contains("S") {
+            result = Int(arr1[i][0])!
+        }else if val.contains("D") {
+            result = Int(arr1[i][0])! * Int(arr1[i][0])!
+        }else if val.contains("T") {
+            result = Int(arr1[i][0])! * Int(arr1[i][0])! * Int(arr1[i][0])!
+        }
+        if val.contains("*") {
+            result = result * 2
+            if i > 0 {
+                arr3[i-1] = arr3[i-1] * 2
+            }
+        }else if val.contains("#") {
+            result = result * -1
+        }
+        arr3[i] = result
+    }
+    return arr3.reduce(0, +)
+}
+extension String {
+    func match(_ regex: String) -> [[String]] {
+        let nsString = self as NSString
+        return (try? NSRegularExpression(pattern: regex, options: []))?.matches(in: self, options: [], range: NSMakeRange(0, nsString.length)).map { match in
+            (0..<match.numberOfRanges).map { match.range(at: $0).location == NSNotFound ? "" : nsString.substring(with: match.range(at: $0)) }
+        } ?? []
+    }
+}
