f51abd11a6fb214e2b981f566fc57f4a0d1891f7
khyeji98 <gpwl981207@naver.com>
2021-08-16 00:17:14 +0900

    update

--- 0819/0819-3/문제-3-Kjh.playground/Contents.swift
+++ 0819/0819-3/문제-3-Kjh.playground/Contents.swift
@@ -1,1 +1,5 @@
 import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    return signs.enumerated().map{return $0.element ? absolutes[$0.offset] : -absolutes[$0.offset]}.reduce(0, +)
+}
