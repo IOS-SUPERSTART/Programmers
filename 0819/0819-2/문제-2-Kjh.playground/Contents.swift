296adc6f40bbfdbd838ecb2452850f958cb87977
khyeji98 <gpwl981207@naver.com>
2021-08-16 00:08:11 +0900

    진짜...?

--- 0819/0819-2/문제-2-Kjh.playground/Contents.swift
+++ 0819/0819-2/문제-2-Kjh.playground/Contents.swift
@@ -1,1 +1,11 @@
 import Foundation
+
+func solution(_ s:String) -> String {
+    return s.components(separatedBy: " ").map{word in return word.enumerated().map{
+        if $0.offset%2 == 0 {
+            return $0.element.uppercased()
+        } else {
+            return $0.element.lowercased()
+        }
+    }.joined()}.joined(separator: " ")
+}
