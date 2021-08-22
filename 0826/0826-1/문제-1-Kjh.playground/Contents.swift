d88d5020bdc47768c9852c79ed85dcf3ebb4dc31
khyeji98 <gpwl981207@naver.com>
2021-08-22 18:57:33 +0900

    1,2 done

--- 0826/0826-1/문제-1-Kjh.playground/Contents.swift
+++ 0826/0826-1/문제-1-Kjh.playground/Contents.swift
@@ -1,1 +1,5 @@
 import Foundation
+
+func solution(_ seoul:[String]) -> String {
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+}
--- 0826/0826-2/문제-2-Kjh.playground/Contents.swift
+++ 0826/0826-2/문제-2-Kjh.playground/Contents.swift
@@ -1,1 +1,9 @@
 import Foundation
+
+func solution(_ n:Int) -> Int {
+    /**
+     10진수는 Int, 그 외 진수는 String
+     String인 3진수로 변환 - 3진수를 reversed() - String인 3진수를 Int인 10진수로 변환
+     */
+    return Int(String(String(n, radix: 3).reversed()), radix: 3)!
+}
