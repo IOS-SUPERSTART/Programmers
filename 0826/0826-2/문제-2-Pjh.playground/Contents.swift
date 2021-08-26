337d71fc8cd073a7629b1a88aa6bfa0025b294ab
zehye <zehye.01@gmail.com>
2021-08-26 12:29:55 +0900

    0826-pjh

--- 0826/0826-2/문제-2-Pjh.playground/Contents.swift
+++ 0826/0826-2/문제-2-Pjh.playground/Contents.swift
@@ -1,1 +1,8 @@
 import Foundation
+
+func solution(_ n:Int) -> Int {
+     let num = String(n,radix: 3)
+    return Int(String(num.reversed()),radix:3)!
+}
+
+solution(34)
--- 0826/0826-3/문제-3-Pjh.playground/Contents.swift
+++ 0826/0826-3/문제-3-Pjh.playground/Contents.swift
@@ -1,1 +1,14 @@
 import Foundation
+
+func solution(_ arr:[Int]) -> Double {
+    var sum: Int = 0
+
+    for i in arr {
+        sum += i
+    }
+
+    return Double(sum) / Double(arr.count)
+}
+
+
+solution([1,3,4,5,6,7])
