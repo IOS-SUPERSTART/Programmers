b8c9eeb5fa61a2f70802e11e9d3b07f21686777d
znfod-dev <znfod.dev@gmail.com>
2021-08-26 14:05:58 +0900

    문제 풀이

--- 0826/0826-1/문제-1-Znf.playground/Contents.swift
+++ 0826/0826-1/문제-1-Znf.playground/Contents.swift
@@ -1,1 +1,16 @@
 import UIKit
+
+func solution(_ seoul:[String]) -> String {
+    var index = 0
+    for (ind, str) in seoul.enumerated() {
+        if str.elementsEqual("Kim") {
+            index = ind
+            break
+        }
+    }
+    return "김서방은 \(index)에 있다"
+}
+
+let test1 = ["Jane", "Kim"]
+
+print(solution(test1))
--- 0826/0826-2/문제-2-Znf.playground/Contents.swift
+++ 0826/0826-2/문제-2-Znf.playground/Contents.swift
@@ -1,1 +1,19 @@
 import UIKit
+
+func solution(_ n:Int) -> Int {
+    return Int(String( String(n, radix: 3).reversed()), radix: 3)!
+    // ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ씨발
+}
+
+let test1 = 45
+let test2 = 125
+let test3 = 3
+let test4 = 1
+let test5 = 100000000
+
+print(solution(test1))
+print(solution(test2))
+print(solution(test3))
+print(solution(test4))
+print(solution(test5))
+
